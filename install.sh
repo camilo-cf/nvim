33d021396ca6b7a3faa7e25cfd14ff79ffb90a74
Camilo A. Cáceres <camilo.caceresf@gmail.com>
2022-02-17 12:24:46 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #/usr/bin/bash
 
-sudo apt-get install vim
-sudo apt-get install neovim
+sudo apt-get install vim neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir ~/.config/nvim
 curl https://raw.githubusercontent.com/camilo-cf/nvim/main/init.vim> ~/.config/nvim/init.vim
