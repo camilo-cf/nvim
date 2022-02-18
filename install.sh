0f3ca9ceea475b438fc7f76b3554bc66fbfe3c68
Camilo A. Cáceres <camilo.caceresf@gmail.com>
2022-02-17 22:33:43 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo apt install neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir ~/.config/nvim
 curl https://raw.githubusercontent.com/camilo-cf/nvim/main/init.vim> ~/.config/nvim/init.vim
-pip3 install pynvim neovim jedi
+pip3 install pynvim neovim jedi debugpy
 mkdir ~/.config/nvim/colors
 curl https://raw.githubusercontent.com/freeo/vim-kalisi/master/colors/kalisi.vim> ~/.config/nvim/colors/kalisi.vim
